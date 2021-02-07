f8409b58cfe3a81d9407eb26d6427f529ccaac47
ahmmedforhad <ahmmedforhad@gmail.com>
2021-02-07 21:46:04 +0600

    ok

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const resultHeading = document.getElementById('result-heading');
 const single_mealEl = document.getElementById('single-meal');
 
 // search meal and fetch from api
-function searchMeal(e){
+function searchMeal(e) {
     e.preventDefault();
 
     // clear single meal
@@ -15,48 +15,57 @@ function searchMeal(e){
     // check for empty
     if (term.trim()) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
-        .then(res => res.json())
-        .then(data => {
-            resultHeading.innerHTML = `<h2>Search result for '${term}:'</h2>`
-            if (data.meals ===  null) {
-                resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
-                
-            } else {
-                meals.innerHTML = data.meals.map(meal =>`
+            .then(res => res.json())
+            .then(data => {
+                resultHeading.innerHTML = `<h2>Search result for '${term}:'</h2>`
+                if (data.meals === null) {
+                    resultHeading.innerHTML = `<p>There are no search results. Try again!</p>`
+
+                } else {
+                    meals.innerHTML = data.meals.map(meal => `
                 <div class = "meal">
                     <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}">
-                    <div class= "meal-info" data-mealId= "${meal.idMeal}">
-                        <h3>${meal.strMeal}</h3>
+                    <div  class= "meal-info" data-mealId= "${meal.idMeal}">
+                        <h3 >${meal.strMeal}</h3>
                     </div> 
                 </div>`)
                 .join('');
-            }
-        });
+                }
+            });
         // clear search text 
         search.value = '';
     } else {
         alert('Please Enter a Meal name');
-        
+
     }
 
 }
-submit.addEventListener('submit',searchMeal)
+submit.addEventListener('submit', searchMeal)
 
 
-fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-.then(res => res.json())
-.then(data => displayCategory(data))
+function displayIngredients() {
+    const displayIngredient = document.getElementById('ingredient');
+    displayIngredients.value = mealId;
+    displayIngredient.addEventListener('click', function () {
 
+        newIngredients();
+    })
 
-function displayCategory(categories){
-    const foodNames = categories.map(category => food.strCategory);
-    const ol = document.getElementById('foods');
-    for (let i = 0; i < foodNames.length; i++) {
-        const food = foodNames[i];
-        const li = document.createElement('li');
-        li.innerText = food;
-        ol.appendChild(li);
-        
-    }
-
+function newIngredients(){
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then(response => response.json())
+    .then(data => {
+        const newData = data.meals
+        newData.forEach(index => {
+            const pop = document.querySelector('.popup')
+            pop.innerHTML = `
+        <h1> Name: ${index.strMeal}</h1>
+        <h6>Ingredient1: ${index.strIngredient1}</h6>
+        <h6>Ingredient2: ${index.strIngredient2}</h6>
+        <h6>Ingredient3: ${index.strIngredient3}</h6>
+        <h6>Ingredient4: ${index.strIngredient4}</h6>
+        <h6>Country Name: ${index.strArea}</h6>`
+        })
+    })
+}
 }
